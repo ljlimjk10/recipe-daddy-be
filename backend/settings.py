a9cb19a35c744d7651a677c7fcf85ac35e22ab77
ljlimjk <ljlimjk10@gmail.com>
2023-10-15 14:41:49 +0800

    [JK] add rest framework and jwt token authentication

--- backend/settings.py
+++ backend/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 
     "recipe_daddy"
 ]
@@ -130,3 +131,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- recipe_daddy/urls.py
+++ recipe_daddy/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from . import views
 
 urlpatterns = [
     path('user/', views.user_views.UserList.as_view(), name='user_view'),
     path('user/<str:username>/', views.user_views.UserDetails.as_view(), name='user_view'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('test', views.test_views.test, name='test_view'),
 ]

357ac7b68d10069ecf71fad11b0c51cd0c020abb
ljlimjk <ljlimjk10@gmail.com>
2023-10-19 16:57:57 +0800

    fix permission input params

--- recipe_daddy/permissions.py
+++ recipe_daddy/permissions.py
@@ -18,7 +18,7 @@ class OwnerOrNoAccessToMealPlan(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_authenticated)
     
-    def has_object_permission(self, request, obj):
+    def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS or request.method == "PUT" or request.method == "DELETE" or request.method == "GET":
             if obj.user.email == request.user.email:
                 return True

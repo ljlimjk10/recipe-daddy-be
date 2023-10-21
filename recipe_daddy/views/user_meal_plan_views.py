bf8e09fd2b7acad3a61c70ea092d8a6b8d72eaea
ljlimjk <ljlimjk10@gmail.com>
2023-10-21 15:00:16 +0800

    [JK] add additional permission check: only owner can view his records

--- recipe_daddy/views/user_meal_plan_views.py
+++ recipe_daddy/views/user_meal_plan_views.py
@@ -1,12 +1,14 @@
 from rest_framework import mixins, generics
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import PermissionDenied
 
 from recipe_daddy.models.user_meal_plan_models import UserMealPlan
 from recipe_daddy.serializers.user_meal_plan_serializers import UserMealPlanSerializer
 from recipe_daddy.helpers.mixins_helpers import MultipleFieldLookupMixin
 from recipe_daddy.permissions import OwnerOrNoAccessToMealPlan
 
+# TODO user shld only have 1 breafast lunch dinner per day max
 
 class UserMealPlanViewSet(
                         MultipleFieldLookupMixin,
@@ -37,9 +39,13 @@ class UserMealPlanViewSet(
             return queryset.filter(pk=pk)
 
         if username is not None:
-            if meal_date is not None:
-                queryset = queryset.filter(user__username=username, meal_date=meal_date)
-            queryset = queryset.filter(user__username=username)
+            if self.request.user.is_authenticated and self.request.user.email == username:
+                if meal_date is not None:
+                    queryset = queryset.filter(user__username=username, meal_date=meal_date)
+                else:
+                    queryset = queryset.filter(user__username=username)
+            else:
+                raise PermissionDenied()
 
         return queryset
 

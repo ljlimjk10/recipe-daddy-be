1222a3ad61e333284d4e224fd26912de86d53790
ljlimjk <ljlimjk10@gmail.com>
2023-10-24 17:59:27 +0800

    [JK] add helper function to generate image based on recipe name

--- recipe_daddy/helpers/user_meal_plan_helpers.py
+++ recipe_daddy/helpers/user_meal_plan_helpers.py
@@ -1,3 +1,8 @@
+import os
+import requests
+from dotenv import load_dotenv
+from urllib.parse import quote
+
 from recipe_daddy.models.user_meal_plan_models import UserMealPlan
 
 def handling_meal_type_existence(user, meal_type, meal_date):
@@ -26,3 +31,37 @@ def get_target_meal(user, meal_type, meal_date):
     ).first()
 
     return target_meal_plan if target_meal_plan else None
+
+
+def generate_meal_image(meal_name):
+    load_dotenv()
+    EDAMAM_APPLICATION_ID = os.getenv("EDAMAM_APPLICATION_ID")
+    EDAMAM_APPLICATION_KEY = os.getenv("EDAMAM_APPLICATION_KEY")
+    print(EDAMAM_APPLICATION_ID, EDAMAM_APPLICATION_KEY)
+    BASE_URL = "https://api.edamam.com/api/recipes/v2"
+    query_params = {
+        "type": "public",
+        "q": meal_name,
+        "app_id": EDAMAM_APPLICATION_ID,
+        "app_key": f"{quote(EDAMAM_APPLICATION_KEY)}",
+        "mealType": "Breakfast",
+        "imageSize": "REGULAR",
+        "random": "true",
+    }
+    headers = {
+            "Content-Type": "application/json"
+        }
+    try:
+        response = requests.get(BASE_URL, headers=headers, params=query_params)
+        if not response.status_code == 200:
+            raise Exception("Unsuccessful request to edamam.")
+        data = response.json()
+        if "hits" in data and data["hits"]:
+            first_hit = data["hits"][0]
+            image_url = first_hit["recipe"]["image"]
+            return image_url, response.status_code
+        else:
+            return None, response.status_code
+        
+    except Exception as _:
+        return None, 404

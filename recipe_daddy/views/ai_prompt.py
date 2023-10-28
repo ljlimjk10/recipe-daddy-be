a5f2c329ce4ac03fee0be33cca3f3f1dfdc0284e
Sean Yap <seanyckang@gmail.com>
2023-10-28 23:40:15 +0800

    Resolved conflict

--- recipe_daddy/views/ai_prompt.py
+++ recipe_daddy/views/ai_prompt.py
@@ -4,6 +4,8 @@ from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from rest_framework import status
 import replicate
+import openai
+import os
 
 @api_view(['POST'])
 def get_ai_prompt(request):
@@ -38,6 +40,3 @@ def get_ai_prompt(request):
         return JsonResponse({'error': str(e)}, status=500)
 
     return JsonResponse({'error': 'Invalid request method'}, status=400)
-def test_shit(request):
-    image_url, _ = generate_meal_image()
-    print(generate_meal_image("scrambled eggs with crab"))

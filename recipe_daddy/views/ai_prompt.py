27f73b027aff4cb147a846d487774a44d1382626
ljlimjk <ljlimjk10@gmail.com>
2023-11-08 18:39:35 +0800

    add in unit conversion for gram

--- recipe_daddy/views/ai_prompt.py
+++ recipe_daddy/views/ai_prompt.py
@@ -1,9 +1,7 @@
-import replicate
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from rest_framework import status
-import replicate
 import openai
 import os
 

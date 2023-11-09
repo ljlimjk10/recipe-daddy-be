890c9f6cf547ea50b746258371db45850a9467ae
ljlimjk <ljlimjk10@gmail.com>
2023-11-09 13:17:48 +0800

    [JK] update wsgi.py to include path for .venv in ec2 instance

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys, site
 
-from django.core.wsgi import get_wsgi_application
+sys.path.insert(0,'/home/ubuntu/dev/proj-be/.venv/lib/python3.9/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
+#activate_env = "/home/ubuntu/dev/proj-be/.venv/bin/activate"
+#exec(open(activate_env).read(), {'__file__': activate_env})
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
